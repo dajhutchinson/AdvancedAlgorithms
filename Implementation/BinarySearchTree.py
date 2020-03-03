c29b97976a840c77a0c71c24d77776a3c4503b45
dahutchinson <dom@domhutchinson.me>
2020-03-03 21:54:22 +0000

    Binary Search - Lookup, predecessor, successor

--- Implementation/BinarySearchTree.py
+++ Implementation/BinarySearchTree.py
@@ -1,7 +1,18 @@
+class Binary_Search_Tree():
+
+    def __init__(self,value:int,left:"Binary_Search_Tree",right:"Binary_Search_Tree"): print("__init__() - Not Implemented"); pass # m is size of table
+    def insert(self,key,value): return "insert() - Not Implemented"
+    def lookup(self,key): return "lookup() - Not Implemented"
+    def predecessor(self,key): return "predecessor() - Not Implemented"
+    def successor(self,key): return "successor() - Not Implemented"
+    def left_rotation(self)->"Binary_Search_Tree": return "left_rotation() - Not Implemented"
+    def right_rotation(self)->"Binary_Search_Tree": return "right_rotation() - Not Implemented"
+    def height(self)->int: return "height() - Not Implemented"
+
 class Balanced_Binary_Search_Tree():
 
     # Insertion Only
-    def __init__(self,v:int,l:int,r:int):
+    def __init__(self,v:int,l:"Balanced_Binary_Search_Tree",r:"Balanced_Binary_Search_Tree"):
         self.v=v
         self.l=l
         self.r=r
@@ -35,6 +46,36 @@ class Balanced_Binary_Search_Tree():
                 return t
         else:
             return self
+
+    # return whether a key is in the tree
+    def lookup(self,key:int)->bool:
+        if key==self.v: return True
+        if (key>self.v and self.r==None): return False
+        elif key>self.v: return self.r.lookup(key)
+        if (key<self.v and self.l==None): return False
+        elif key<self.v: return self.l.lookup(key)
+
+    # return predecessor to key in the tree, if one exists
+    def predecessor(self,key:int)->int:
+        if key==self.v: return key
+        if (self.v<key):
+            if (self.r==None): return self.v
+            if (self.r.v>key): return self.v
+            return self.r.predecessor(key)
+        else:
+            if (self.l==None): return None
+            return self.l.predecessor(key)
+
+    # return successor to key in the tree, if one exists
+    def successor(self,key:int)->int:
+        if key==self.v: return key
+        if (self.v>key):
+            if (self.l==None): return self.v
+            if (self.l.v<key): return self.v
+            return self.l.successor(key)
+        else:
+            if (self.r==None): return None
+            return self.r.successor(key)
 
     # return height of tree
     def height(self)->int:
@@ -81,6 +122,7 @@ class Balanced_Binary_Search_Tree():
         pivot.r=self
         return pivot
 
+    # build balanced tree for list of integers
     def build_tree(data:[int])->"Balanced_Binary_Search_Tree":
         tree=Balanced_Binary_Search_Tree(data[0],None,None)
         for i in data[1:]:
@@ -103,6 +145,12 @@ class Balanced_Binary_Search_Tree():
         return "v:{}\n  l:{}\n  r:{}".format(self.v,l_str,r_str)
 
 if __name__=="__main__":
-    array=[i for i in range(15)]
+    array=[i for i in range(0,15,2)]
     tree=Balanced_Binary_Search_Tree.build_tree(array)
     print(tree)
+    print(tree.lookup(14))
+    print(tree.lookup(10))
+    print(tree.predecessor(-1))
+    print(tree.predecessor(15))
+    print(tree.successor(-1))
+    print(tree.successor(15))
